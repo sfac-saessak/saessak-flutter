e8a42a3c2080fc12d527edebb35316f5b1cd3487
gong <ddddy0114@gmail.com>
2023-06-08 05:02:54 +0900

    설정탭 유저 프로필

--- lib/view/screen/setting_screen.dart
+++ lib/view/screen/setting_screen.dart
@@ -1,8 +1,11 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../controller/auth_controller.dart';
+import '../../util/app_color.dart';
+import '../../util/app_routes.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -12,7 +15,40 @@ class SettingScreen extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('setting'),
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              child: CircleAvatar(
+                backgroundColor: AppColor.black20,
+                backgroundImage:
+                  FirebaseAuth.instance.currentUser!.photoURL != null
+                    ? NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!)
+                    : null,
+                child:
+                  FirebaseAuth.instance.currentUser!.photoURL != null
+                    ? null
+                    : Icon(Icons.person, color: AppColor.white),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('${FirebaseAuth.instance.currentUser!.displayName}'),
+                Text('${FirebaseAuth.instance.currentUser!.email}'),
+              ],
+            ),
+            Spacer(),
+            IconButton(
+              onPressed: (){
+                // Get.toNamed(AppRoutes.setName);
+              },
+              icon: Icon(Icons.edit),
+            ),
+            SizedBox(width: 30),
+          ],
+        ),
         Padding(
           padding: const EdgeInsets.only(left: 20, right: 20, top: 100),
           child: Container(
