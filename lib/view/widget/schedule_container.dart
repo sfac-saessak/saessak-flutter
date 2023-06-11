841574074818a82c667769a58c52fe782a80875a
aPPbARRRR <134025007+aPPbARRRR@users.noreply.github.com>
2023-06-11 20:47:21 +0900

    일정 버튼 배치 수정

--- lib/view/widget/schedule_container.dart
+++ lib/view/widget/schedule_container.dart
@@ -27,54 +27,55 @@ class ScheduleContainer extends StatelessWidget {
             ischecked: e.isExecuted,
             data: ScheduleCompanion(isExecuted: drift.Value(!e.isExecuted)),
           ),
-          GestureDetector(
-            onTap: () => Get.dialog(
-              CustomDialog(
-                child: Column(children: [
-                  Text('이름: ${e.plant}'),
-                  Text('날짜: ${e.year}년 ${e.month}월 ${e.day}일'),
-                  Text('시간: ${e.time} 시'),
-                  Text('내용: ${e.content}'),
-                ]),
-                leftButtonText: '일정수정',
-                rightButtonText: '일정삭제',
-                leftButtonOnTap: () =>
-                    Get.find<ScheduleController>().modifyScheduleDialog(e),
-                rightButtonOnTap: () {
-                  Get.find<ScheduleController>().deleteSchedule(e.id);
-                  Get.back();
-                },
+          Row(
+            children: [
+              Text(
+                '[${e.plant}]',
+                style: AppTextStyle.body2_m()
+                    .copyWith(color: e.isExecuted ? AppColor.black30 : null),
               ),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  '[${e.plant}]',
-                  style: AppTextStyle.body2_m()
-                      .copyWith(color: e.isExecuted ? AppColor.black30 : null),
-                ),
-                Text(
-                  e.content,
-                  style: AppTextStyle.body2_m()
-                      .copyWith(color: e.isExecuted ? AppColor.black30 : null),
-                ),
-                // 알림설정 켜고 끄는 버튼
-                IconButton(
-                    onPressed: () {
-                      // 디비에 다시 저장 후 불러와 화면 리빌드하기
-                      Get.find<ScheduleController>().onTapNotifyButton(
-                          e,
-                          ScheduleCompanion(
-                              isDoNotify: drift.Value(!e.isDoNotify)));
-                    },
-                    icon: e.isDoNotify
-                        ? Icon(Icons.notifications)
-                        : Icon(
-                            Icons.notifications_off,
-                            color: AppColor.black30,
-                          ))
-              ],
-            ),
+              Text(
+                e.content,
+                style: AppTextStyle.body2_m()
+                    .copyWith(color: e.isExecuted ? AppColor.black30 : null),
+              ),
+              // 알림 on-off 버튼
+              IconButton(
+                  onPressed: e.isExecuted
+                      ? () {}
+                      : () {
+                          // 디비에 다시 저장 후 불러와 화면 리빌드하기
+                          Get.find<ScheduleController>().onTapNotifyButton(
+                              e,
+                              ScheduleCompanion(
+                                  isDoNotify: drift.Value(!e.isDoNotify)));
+                        },
+                  icon: e.isDoNotify
+                      ? Icon(Icons.notifications,
+                          color: e.isExecuted ? AppColor.black30 : null)
+                      : Icon(
+                          Icons.notifications_off,
+                          color: AppColor.black30,
+                        )),
+              // 수정버튼
+              IconButton(
+                  onPressed: e.isExecuted
+                      ? () {}
+                      : () => Get.find<ScheduleController>()
+                          .modifyScheduleDialog(e),
+                  icon: Icon(
+                    Icons.edit,
+                    color: e.isExecuted ? AppColor.black30 : null,
+                  )),
+              // 삭제버튼
+              IconButton(
+                  onPressed: () {
+                    Get.find<ScheduleController>().deleteSchedule(e.id);
+                    Get.back();
+                  },
+                  icon: Icon(Icons.delete,
+                      color: e.isExecuted ? AppColor.black30 : null))
+            ],
           )
         ],
       ),
