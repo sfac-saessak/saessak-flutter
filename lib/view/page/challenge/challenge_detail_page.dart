a8daa30a5cb51aa4685a0391f06b25e4d9e099be
2Juzzang <skyy630@naver.com>
2023-06-12 17:57:09 +0900

    fix) 챌린지 상세 사진, 글 위치 수정

--- lib/view/page/challenge/challenge_detail_page.dart
+++ lib/view/page/challenge/challenge_detail_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,14 +11,16 @@ import '../../../util/app_text_style.dart';
 import 'edit_challenge_page.dart';
 
 class ChallengeDetailPage extends StatelessWidget {
-  const ChallengeDetailPage({Key? key, required this.challenge, required this.challengeEnd}) : super(key: key);
+  const ChallengeDetailPage(
+      {Key? key, required this.challenge, required this.challengeEnd})
+      : super(key: key);
   final Challenge challenge;
   final bool challengeEnd;
 
   @override
   Widget build(BuildContext context) {
     var controller = Get.find<ChallengeController>();
-    
+
     return Scaffold(
       appBar: AppBar(
         title: Text('챌린지'),
@@ -27,16 +28,19 @@ class ChallengeDetailPage extends StatelessWidget {
         backgroundColor: AppColor.white,
         foregroundColor: AppColor.black,
         elevation: 0,
-        actions: controller.user.uid == challenge.admin ? [
-          IconButton(
-            onPressed: () => Get.off(() => EditChallengePage(challenge: challenge)),
-            icon: Icon(Icons.edit),
-          ),
-          // IconButton(
-          //   onPressed: (){},
-          //   icon: Icon(Icons.delete),
-          // ),
-        ] : null,
+        actions: controller.user.uid == challenge.admin
+            ? [
+                IconButton(
+                  onPressed: () =>
+                      Get.off(() => EditChallengePage(challenge: challenge)),
+                  icon: Icon(Icons.edit),
+                ),
+                // IconButton(
+                //   onPressed: (){},
+                //   icon: Icon(Icons.delete),
+                // ),
+              ]
+            : null,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -61,12 +65,10 @@ class ChallengeDetailPage extends StatelessWidget {
                 Text('${challenge.members!.length}/${challenge.memberLimit}'),
                 Spacer(),
                 challengeEnd
-                  ? Text('모집 마감')
-                  : Text(
-                  controller.getDeadline(challenge.startDate) == 0
-                    ? '마감 D-DAY'
-                    : '마감 D${controller.getDeadline(challenge.startDate)}'
-                )
+                    ? Text('모집 마감')
+                    : Text(controller.getDeadline(challenge.startDate) == 0
+                        ? '마감 D-DAY'
+                        : '마감 D${controller.getDeadline(challenge.startDate)}')
               ],
             ),
             Row(
@@ -74,65 +76,64 @@ class ChallengeDetailPage extends StatelessWidget {
                 Icon(Icons.access_time, size: 16),
                 SizedBox(width: 4),
                 Text(
-                  '${DateFormat("yyyy-MM-dd").format(challenge.startDate.toDate())} ~ ${DateFormat("yyyy-MM-dd").format(challenge.endDate.toDate())}'
-                ),
+                    '${DateFormat("yyyy-MM-dd").format(challenge.startDate.toDate())} ~ ${DateFormat("yyyy-MM-dd").format(challenge.endDate.toDate())}'),
               ],
             ),
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
+            challenge.imageUrl != null
+                ? Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.grey[300],
+                    child:
+                        Image.network(challenge.imageUrl!, fit: BoxFit.cover))
+                : Container(),
             Text('${challenge.content}'),
             Spacer(),
-            challenge.imageUrl != null
-              ? Container(
-                width: double.infinity,
-                height: 150,
-                color: Colors.grey[300],
-                child: Image.network(challenge.imageUrl!, fit: BoxFit.cover)
-              )
-              : Container(),
             Container(
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
                   challengeEnd
-                    ? Get.snackbar('참가 실패', '모집이 끝난 챌린지입니다.')
-                    : showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text("참가"),
-                            content:
-                            Text('${challenge.title}에 참가하시겠습니까?'),
-                            actions: [
-                              IconButton(
-                                onPressed: () => Get.back(),
-                                icon: const Icon(
-                                  Icons.cancel,
-                                  color: Colors.red,
+                      ? Get.snackbar('참가 실패', '모집이 끝난 챌린지입니다.')
+                      : showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("참가"),
+                              content: Text('${challenge.title}에 참가하시겠습니까?'),
+                              actions: [
+                                IconButton(
+                                  onPressed: () => Get.back(),
+                                  icon: const Icon(
+                                    Icons.cancel,
+                                    color: Colors.red,
+                                  ),
                                 ),
-                              ),
-                              IconButton(
-                                onPressed: () async {
-                                  controller.joinChallenge(challenge.challengeId!);
-                                  Get.snackbar('${challenge.title}', '참가 완');
-                                },
-                                icon: const Icon(
-                                  Icons.done,
-                                  color: Colors.green,
+                                IconButton(
+                                  onPressed: () async {
+                                    controller
+                                        .joinChallenge(challenge.challengeId!);
+                                    Get.snackbar('${challenge.title}', '참가 완');
+                                  },
+                                  icon: const Icon(
+                                    Icons.done,
+                                    color: Colors.green,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
-                      }
-                  );
+                              ],
+                            );
+                          });
                 },
                 child: Text('참가하기'),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-
