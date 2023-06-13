1406ebe63127bbd83b32adc4d5466205446b345c
2Juzzang <skyy630@naver.com>
2023-06-13 17:12:44 +0900

    fix) 문의하기 ios, android 구동확인, 설정파일

--- lib/controller/setting_controller.dart
+++ lib/controller/setting_controller.dart
@@ -1,9 +1,9 @@
-
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../model/community/post.dart';
 import '../model/notice.dart';
@@ -15,12 +15,12 @@ import 'schedule_journal/journal_controller.dart';
 class SettingController extends GetxController {
   User get user => FirebaseAuth.instance.currentUser!;
 
-  RxList journalList = Get.find<JournalController>().journalList;      // 일지 리스트
-  RxList followingList = Get.find<FriendsController>().followingList;  // 팔로잉 리스트
-  RxList followerList = [].obs;   // 팔로워 리스트
-  RxList myPostList = [].obs;     // 게시글 리스트
-  RxBool isLoading = false.obs;   // 로딩중 상태
-  RxList<Notice> noticeList = <Notice>[].obs;     // 공지 리스트
+  RxList journalList = Get.find<JournalController>().journalList; // 일지 리스트
+  RxList followingList = Get.find<FriendsController>().followingList; // 팔로잉 리스트
+  RxList followerList = [].obs; // 팔로워 리스트
+  RxList myPostList = [].obs; // 게시글 리스트
+  RxBool isLoading = false.obs; // 로딩중 상태
+  RxList<Notice> noticeList = <Notice>[].obs; // 공지 리스트
 
   // 팔로워 가져오기
   getFollower() async {
@@ -41,7 +41,9 @@ class SettingController extends GetxController {
     isLoading(true);
     myPostList([]);
     QuerySnapshot snapshot = await DBService().getUserPosts(user.uid);
-    myPostList(snapshot.docs.map((doc) => Post.fromMap(doc.data() as Map<String, dynamic>)).toList());
+    myPostList(snapshot.docs
+        .map((doc) => Post.fromMap(doc.data() as Map<String, dynamic>))
+        .toList());
     log('${myPostList}');
     isLoading(false);
   }
@@ -56,8 +58,24 @@ class SettingController extends GetxController {
   readNotice() async {
     isLoading(true);
     QuerySnapshot snapshot = await DBService().readNotice();
-    noticeList(snapshot.docs.map((doc) => Notice.fromMap(doc.data() as Map<String, dynamic>)).toList());
+    noticeList(snapshot.docs
+        .map((doc) => Notice.fromMap(doc.data() as Map<String, dynamic>))
+        .toList());
     isLoading(false);
+  }
+
+  //문의 메일 보내기
+  launchEmail() async {
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: 'kongdo114@gmail.com',
+    );
+    final String url = params.toString();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   @override
