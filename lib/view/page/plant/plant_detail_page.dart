8cdfcb8fcf4c75b239575c78e7e90cd2e65fb26f
gong <ddddy0114@gmail.com>
2023-06-13 12:10:14 +0900

    식물 상세 갤러리 수정

--- lib/view/page/plant/plant_detail_page.dart
+++ lib/view/page/plant/plant_detail_page.dart
@@ -19,8 +19,9 @@ class PlantDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     print('플랜트 상세 : ${plant}');
     var controller = Get.find<PlantController>();
+    var journalController = Get.find<JournalController>();
     
-    RxList<Journal> journalList = Get.find<JournalController>().journalList;
+    RxList<Journal> journalList = journalController.journalList;
     List galleryJournal = [];
     for (Journal journal in journalList) {
       if (plant.plantId == journal.plant.plantId) {
@@ -30,105 +31,111 @@ class PlantDetailPage extends StatelessWidget {
       }
     }
     
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${plant.name}'),
-        centerTitle: true,
-        backgroundColor: AppColor.white,
-        foregroundColor: AppColor.black,
-        elevation: 0,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Get.off(() => AddPlantPage(plant: plant));
-            },
-            icon: Icon(Icons.edit),
-          ),
-          IconButton(
-            onPressed: () {
-              controller.deletePlant(plant.plantId!);
-            },
-            icon: Icon(Icons.delete),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                child: CircleAvatar(
-                  backgroundColor: AppColor.black20,
-                  backgroundImage: plant.imageUrl != null
-                      ? NetworkImage(plant.imageUrl!)
-                      : null,
-                  child: plant.imageUrl != null
-                      ? null
-                      : Icon(Icons.person, color: AppColor.white),
-                ),
-              ),
-              Column(
-                children: [
-                  Text('${plant.name}'),
-                  Text('${plant.species}'),
-                  Row(
-                    children: [
-                      Text(
-                          '${DateFormat("yyyy-MM-dd").format(plant.plantingDate.toDate())}'),
-                      Text(
-                          '(D+${controller.getDaysSincePlanting(plant.plantingDate)})'),
-                    ],
-                  ),
-                ],
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Text('급수 주기:'),
-              Text('${plant.wateringCycle}'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('최적 온도:'),
-              Text('${plant.optimalTemperature}'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('빛 요구도:'),
-              Text('${plant.lightRequirement}'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('메모:'),
-              Text('${plant.memo}'),
-            ],
-          ),
-
-          Text('갤러리'),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 16,
-              ),
-              itemCount: galleryJournal.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Get.to(() => JournalDetailPage(journal: galleryJournal[index]));
-                  },
-                  child: Image.network(galleryJournal[index].imageUrl),
-                );
+    return WillPopScope(
+      onWillPop: () {
+        journalController.onDelete();
+        return Future.value(true);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('${plant.name}'),
+          centerTitle: true,
+          backgroundColor: AppColor.white,
+          foregroundColor: AppColor.black,
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Get.off(() => AddPlantPage(plant: plant));
               },
+              icon: Icon(Icons.edit),
             ),
-          )
-        ],
+            IconButton(
+              onPressed: () {
+                controller.deletePlant(plant.plantId!);
+              },
+              icon: Icon(Icons.delete),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  child: CircleAvatar(
+                    backgroundColor: AppColor.black20,
+                    backgroundImage: plant.imageUrl != null
+                        ? NetworkImage(plant.imageUrl!)
+                        : null,
+                    child: plant.imageUrl != null
+                        ? null
+                        : Icon(Icons.person, color: AppColor.white),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text('${plant.name}'),
+                    Text('${plant.species}'),
+                    Row(
+                      children: [
+                        Text(
+                            '${DateFormat("yyyy-MM-dd").format(plant.plantingDate.toDate())}'),
+                        Text(
+                            '(D+${controller.getDaysSincePlanting(plant.plantingDate)})'),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text('급수 주기:'),
+                Text('${plant.wateringCycle}'),
+              ],
+            ),
+            Row(
+              children: [
+                Text('최적 온도:'),
+                Text('${plant.optimalTemperature}'),
+              ],
+            ),
+            Row(
+              children: [
+                Text('빛 요구도:'),
+                Text('${plant.lightRequirement}'),
+              ],
+            ),
+            Row(
+              children: [
+                Text('메모:'),
+                Text('${plant.memo}'),
+              ],
+            ),
+
+            Text('갤러리'),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 16,
+                ),
+                itemCount: galleryJournal.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Get.to(() => JournalDetailPage(journal: galleryJournal[index]));
+                    },
+                    child: Image.network(galleryJournal[index].imageUrl),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
